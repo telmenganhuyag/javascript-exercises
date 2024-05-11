66ff0cc38edae66cb98c6e27eb713a8ef04f6807
Telmen <telmenganhuyag@gmail.com>
2024-05-11 15:48:32 +0800

    object errrors

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,30 +1,33 @@
 const add = function(x, y) {
-	let sum = x + y;
-  return sum
+	return x + y;
 };
 
 const subtract = function(x, y) {
-	let sum = x - y;
-  return sum
+	return x - y;
 };
 
-const sum = function([]) {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function(x, y) {
-  let sum = x * y;
-  return sum
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
 const power = function(x, y) {
-	let sum = x + y;
-  return sum
+  let result = 1;
+  for (let i = 1; i <= y; i++) { 
+    result *= x;
+  }
+  return result;
 };
 
 const factorial = function(x) {
-	let sum = x + y;
-  return sum
+	let result = 1;
+  for (let i = 1; i <= x; i++) { 
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
