92d5aca7c7aec101e2a24a28814ec63c93a2ccee
Telmen <telmenganhuyag@gmail.com>
2024-05-11 19:44:29 +0800

    reduce is amaze

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+// const findTheOldest = function(people) {
+// return people.map((person) => ({ 
+//     name: person.name,
+//     age: person.yearOfDeath - person.yearOfBirth
+// }))
+// .reduce((oldest, current) => (current.age > oldest.age) ? current : oldest);
+// };
 
-};
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.map((person) => ({ 
+        name: person.name,
+        age: (person.yearOfDeath || currentYear) - person.yearOfBirth
+    }))
+    .reduce((oldest, current) => {
+      return (current.age > oldest.age) ? current : oldest;
+    });
+  };
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
