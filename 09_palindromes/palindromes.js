5c2a7f7e28e0c73b0f2f6cc942eec158d44410bf
Telmen <telmenganhuyag@gmail.com>
2024-05-11 17:01:15 +0800

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const cleanedString = str
+.toLowerCase()
+.split('')
+.filter((character) => alphanumerical.includes(character))
+.join('');
 
+const reversedString = cleanedString.split('').reverse('').join('');
+
+return cleanedString === reversedString;
 };
 
 // Do not edit below this line
